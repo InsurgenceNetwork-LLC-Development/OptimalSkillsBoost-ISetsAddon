1ae8c9772e047c0bb14416bce2585ba854b52f0b
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 18:53:23 -0500

    fixed boost give incorrect amounts

--- src/main/java/org/insurgencedev/optimalskillsboost/OptimalSkillBoostAddon.java
+++ src/main/java/org/insurgencedev/optimalskillsboost/OptimalSkillBoostAddon.java
@@ -3,18 +3,16 @@ package org.insurgencedev.optimalskillsboost;
 import org.bukkit.Bukkit;
 import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
 import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
+import org.insurgencedev.optimalskillsboost.listeners.SkillExperienceGainListener;
 
-@ISetsAddon(name = "OptimalSkillsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the skill experience earned from OptimalSkills")
+@ISetsAddon(name = "OptimalSkillsBoost", version = "1.0.1", author = "Insurgence Dev Team", description = "Boost the skill experience earned from OptimalSkills")
 public class OptimalSkillBoostAddon extends InsurgenceSetsAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        if (isDependentEnabled()) {
+        if (Bukkit.getPluginManager().isPluginEnabled("OptimalSkills")) {
             registerEvent(new SkillExperienceGainListener());
         }
     }
 
-    private boolean isDependentEnabled() {
-        return Bukkit.getPluginManager().isPluginEnabled("OptimalSkills");
-    }
 }
---
+++ src/main/java/org/insurgencedev/optimalskillsboost/listeners/SkillExperienceGainListener.java
@@ -0,0 +1,82 @@
+package org.insurgencedev.optimalskillsboost.listeners;
+
+import gg.optimalgames.skills.api.events.SkillExperienceGainEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.insurgencedev.insurgencesets.api.ISetsAPI;
+import org.insurgencedev.insurgencesets.api.contracts.IArmorSet;
+import org.insurgencedev.insurgencesets.api.contracts.IPlayer;
+import org.insurgencedev.insurgencesets.data.ArmorSetData;
+import org.insurgencedev.insurgencesets.libs.fo.remain.nbt.NBTItem;
+import org.insurgencedev.insurgencesets.models.upgrade.Boost;
+import org.insurgencedev.insurgencesets.models.upgrade.Upgrade;
+
+public class SkillExperienceGainListener implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    public void onGain(SkillExperienceGainEvent event) {
+        Player player = event.getSkillProfile().getPlayer();
+        IPlayer cache = ISetsAPI.getCache(player);
+        long totalAmount = 0;
+
+        for (ItemStack item : player.getInventory().getArmorContents()) {
+            if (item == null || item.getType().isAir()) {
+                continue;
+            }
+
+            NBTItem nbtItem = new NBTItem(item);
+            if (!nbtItem.hasTag("armorSet")) {
+                continue;
+            }
+
+            String armorSetName = nbtItem.getString("armorSet");
+            if (!ISetsAPI.getArmorSetManager().isArmorSetValid(armorSetName)) {
+                continue;
+            }
+
+            String itemType = item.getType().name().split("_")[1];
+            ArmorSetData data = cache.getArmorSetDataManager().getArmorSetData(armorSetName);
+            if (data == null) {
+                continue;
+            }
+
+            IArmorSet armorSet = ISetsAPI.getArmorSetManager().getArmorSet(armorSetName);
+            Upgrade upgrade = armorSet != null ? armorSet.getArmorPieceManager().findPieceLevels(itemType, getLevel(itemType, data)) : null;
+            if (upgrade == null) {
+                continue;
+            }
+
+            for (Boost boost : upgrade.getBoosts()) {
+                if (boost.getNamespace().equals("OPTIMAL_SKILLS") && boost.getType().equals("Skills")) {
+                    double boostAmount = boost.getBOOST_SETTINGS().getDouble("Boost_Amount");
+                    totalAmount += calcAmount(event.getExperience(), boost.isPercent(), boostAmount);
+                }
+            }
+        }
+
+        if (totalAmount > 0) {
+            event.setExperience(totalAmount);
+        }
+    }
+
+    private long calcAmount(long amountFromEvent, boolean isPercent, double boostAmount) {
+        if (isPercent) {
+            return (long) (amountFromEvent * (1 + boostAmount / 100));
+        } else {
+            return (long) (amountFromEvent * (boostAmount < 1 ? 1 + boostAmount : boostAmount));
+        }
+    }
+
+    private int getLevel(String type, ArmorSetData armorSetData) {
+        return switch (type) {
+            case "HEAD", "HELMET" -> armorSetData.getHelmetLevels();
+            case "CHESTPLATE" -> armorSetData.getChestplateLevels();
+            case "LEGGINGS" -> armorSetData.getLeggingsLevels();
+            case "BOOTS" -> armorSetData.getBootsLevels();
+            default -> 0;
+        };
+    }
+
+}
